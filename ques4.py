567a90aa21cd3c627cc9797ab6ac1283dfa87076
DESKTOP-5K41LSK <mahnoor2809@gmail.com>
2019-11-19 22:59:50 +0500

    Python Code

---
+++ ques4.py
@@ -0,0 +1,9 @@
+print(" \n Participants' Score_Sheet Of University Sports Day  ")
+scores = [8, 5, 6, 7, 4, 6, 8, 7]
+print(scores[:])
+print(" Runner-Up Score : ")
+r = max(scores)
+#print(max(scores))
+while max(scores) == r:
+    scores.remove(max(scores))
+print(max(scores))    
