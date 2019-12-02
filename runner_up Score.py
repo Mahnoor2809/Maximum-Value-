5f3c87281809c591ba5f26139c58022be8edf71e
Mahnoor Hafeez <50525665+Mahnoor2809@users.noreply.github.com>
2019-12-02 23:22:50 +0500

    Update runner_up Score.py

--- runner_up Score.py
+++ runner_up Score.py
@@ -3,7 +3,7 @@ scores = [8, 5, 6, 7, 4, 6, 8, 7]
 print(scores[:])
 print(" Runner-Up Score : ")
 r = max(scores)
-#print(max(scores))
+#print(max(scores))     #For maximum Score
 while max(scores) == r:
     scores.remove(max(scores))
-print(max(scores))    
+print(max(scores))      #For Runner Up Score
